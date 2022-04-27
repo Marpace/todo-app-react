5edb9bf6082352caa867bbd4680d55561e9fd41e
Marpace <almonacid.pablo@hotmail.com>
2022-04-27 14:14:27 -0400

    changed path for images

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,14 +15,14 @@ function Header(props){
             <h1>TODO</h1>
             <button className={props.theme === "light" ? "show" : "hidden"} onClick={handleClick}>
                 <img 
-                src="/images/icon-moon.svg" 
+                src="public/images/icon-moon.svg" 
                 alt="half moon"
                 id="dark">
                 </img>
             </button>
             <button className={props.theme === "dark" ? "show" : "hidden"} onClick={handleClick} >
                 <img 
-                src="/images/icon-sun.svg" 
+                src="public/images/icon-sun.svg" 
                 alt="sun"
                 id="light"></img>
             </button>
